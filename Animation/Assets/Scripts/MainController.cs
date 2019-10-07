52b9167dee0d799a5e25c7fd506c5cd081839026
tonnac35 <tonnac35@gmail.com>
2019-10-07 22:00:21 +0900

    Animation

---
+++ Animation/Assets/Scripts/MainController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody rigidBody;
+
+    private float h;
+    private float v;
+
+    private float moveX;
+    private float moveZ;
+    private float speedH = 50f;
+    private float speedZ = 80f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.Play("JUMP00", -1, 0);
+        }
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("h", h);
+        animator.SetFloat("v", v);
+
+        moveX = h * speedH * Time.deltaTime;
+        moveZ = v * speedZ * Time.deltaTime;
+
+        if(moveZ <= 0)
+        {
+            moveX = 0;
+        }
+
+        rigidBody.velocity = new Vector3(moveX, 0, moveZ);
+    }
+}
