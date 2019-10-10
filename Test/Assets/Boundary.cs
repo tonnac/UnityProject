8ab4143ebfe58dd1d40e04a7ffcde0083b3184a4
tonnac <tonnac@ilati.com>
2019-10-10 09:48:56 +0900

    test

---
+++ Test/Assets/Boundary.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Boundaray Enter");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Boundaray Exit");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Enter");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exit");
+    }
+}
---
+++ Test/Assets/bC.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bC : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision Exit");
+    }
+}
