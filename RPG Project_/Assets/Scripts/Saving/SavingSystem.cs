deceb35f1b14c1abc57c764b403cc7d71f811e6e
tonnac35 <tonnac35@gmail.com>
2019-11-12 01:25:11 +0900

    90. Understanding Unicode

--- RPG Project_/Assets/Scripts/Saving/SavingSystem.cs
+++ RPG Project_/Assets/Scripts/Saving/SavingSystem.cs
@@ -1,13 +1,22 @@
 namespace RPG.Saving
 {
     using System.IO;
+    using System.Text;
     using UnityEngine;
     
     public class SavingSystem : MonoBehaviour 
     {
         public void Save(string saveFile)
         {
-            print($"Saving to {GetPathFromSaveFile(saveFile)}");
+            string path = GetPathFromSaveFile(saveFile);
+            print($"Saving to {path}");
+            using(FileStream stream = File.Open(path, FileMode.Create))
+            {
+                stream.WriteByte(0xc2);
+                stream.WriteByte(0xa1);
+                byte[] bytes = Encoding.UTF8.GetBytes("Hola Mundo!");
+                stream.Write(bytes, 0, bytes.Length);
+            }
         }
 
         public void Load(string saveFile)
