70ac2e0946ec0dafdd2debe741439b2da0fb997c
tonnac35 <tonnac35@gmail.com>
2019-11-13 03:43:03 +0900

    93. BinaryFormatter For Serialization

--- RPG Project_/Assets/Scripts/Saving/SavingSystem.cs
+++ RPG Project_/Assets/Scripts/Saving/SavingSystem.cs
@@ -1,6 +1,8 @@
 namespace RPG.Saving
 {
+    using System;
     using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
     using System.Text;
     using UnityEngine;
     
@@ -11,8 +13,10 @@ namespace RPG.Saving
             print($"Saving to {GetPathFromSaveFile(saveFile)}");
             using(FileStream stream = File.Open(GetPathFromSaveFile(saveFile), FileMode.Create))
             {
-                byte[] bytes = Encoding.UTF8.GetBytes("Hola Mundo!");
-                stream.Write(bytes, 0, bytes.Length);
+                Transform playerTransform = GetPlayerTransform();
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 position = new SerializableVector3(playerTransform.position);
+                formatter.Serialize(stream, position);
             }
         }
 
@@ -21,21 +25,35 @@ namespace RPG.Saving
             print($"Loading from {GetPathFromSaveFile(saveFile)}");
             using(FileStream stream = File.Open(GetPathFromSaveFile(saveFile), FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
-                print(Encoding.UTF8.GetString(buffer));
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 position = (SerializableVector3)formatter.Deserialize(stream);
+                GetPlayerTransform().position = position.ToVector();
             }
         }
 
-        // private byte[] SerializeVector(Vector3 vector)
-        // {
+        
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
 
-        // }
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte [] vectorBytes = new byte[3 * sizeof(float)];
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
+            return vectorBytes;
+        }
 
-        // private Vector3 DeserializeVector(byte[] buffer)
-        // {
-
-        // }
+        private Vector3 DeserializeVector(byte[] buffer)
+        {
+            Vector3 result = new Vector3();
+            result.x = BitConverter.ToSingle(buffer, 0);
+            result.y = BitConverter.ToSingle(buffer, 4);
+            result.z = BitConverter.ToSingle(buffer, 8);
+            return result;
+        }
 
         private string GetPathFromSaveFile(string saveFile)
         {
