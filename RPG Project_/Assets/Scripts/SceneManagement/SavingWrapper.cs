901cd17a5f734f8c1fdf41ed9601df7ec23616dd
tonnac <tonnac@ilati.com>
2019-11-14 12:26:31 +0900

    105. Fading Before Scene Load

--- RPG Project_/Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ RPG Project_/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -1,15 +1,20 @@
-namespace RPG.Saving
+namespace RPG.SceneManagement
 {
     using System.Collections;
+    using RPG.Saving;
     using UnityEngine;
     
     public class SavingWrapper : MonoBehaviour 
     {
         const string defaultSaveFile = "save";
+        [SerializeField] float fadeInTime = .5f;
 
         private IEnumerator Start() 
         {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
             yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
         }
         private void Update() 
         {
