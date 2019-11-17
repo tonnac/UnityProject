d94a156f9bb02181c3f4ae0e962e7ca05a9c70b0
tonnac35 <tonnac35@gmail.com>
2019-11-18 02:49:34 +0900

    137. Performant Lookups With Dictionaries

--- RPG Project_/Assets/Scripts/Stats/Progression.cs
+++ RPG Project_/Assets/Scripts/Stats/Progression.cs
@@ -1,5 +1,7 @@
 ﻿namespace RPG.Stats
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
     using UnityEngine;
     
@@ -7,8 +9,17 @@
     public class Progression : ScriptableObject 
     {
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
+
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
+            BuildLookup();
+            float[] levels = lookupTable[characterClass][stat];
+
+            return levels.Length >= level ? levels[level - 1] : 0;
+
             // foreach (ProgressionCharacterClass progressionClass in characterClasses)
             // {
             //     if(progressionClass.characterClass != characterClass) continue;
@@ -22,25 +33,44 @@
             //     }
             // }
 
-            var a = from progressionClass in characterClasses 
-            where progressionClass.characterClass == characterClass
-            select progressionClass.stats;
+            // var a = from progressionClass in characterClasses 
+            // where progressionClass.characterClass == characterClass
+            // select progressionClass.stats;
 
-            if(a.Count() < 1)
+            // if(a.Count() < 1)
+            // {
+            //     Debug.LogError($"{characterClass}, stats is null");
+            // }
+
+            // var b = from progressionStat in a.Single()
+            // where progressionStat.stat == stat && progressionStat.levels.Length >= level
+            // select progressionStat.levels[level - 1];
+
+            // if(b.Count() < 1)
+            // {
+            //     Debug.LogError($"{stat} of {characterClass}, Level: {level} is null");
+            // }
+
+            // return b.SingleOrDefault();
+        }
+
+        private void BuildLookup()
+        {
+            if(null != lookupTable) return;
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+            foreach (ProgressionCharacterClass progressionClass in characterClasses)
             {
-                Debug.LogError($"{characterClass}, stats is null");
+                Dictionary<Stat, float[]> statLookupTable = new Dictionary<Stat, float[]>();
+
+                foreach (ProgressionStat progressionStat in progressionClass.stats)
+                {
+                    statLookupTable[progressionStat.stat] = progressionStat.levels;
+                }
+
+                lookupTable[progressionClass.characterClass] = statLookupTable;
             }
-
-            var b = from progressionStat in a.Single()
-            where progressionStat.stat == stat && progressionStat.levels.Length >= level
-            select progressionStat.levels[level - 1];
-
-            if(b.Count() < 1)
-            {
-                Debug.LogError($"{stat} of {characterClass}, Level: {level} is null");
-            }
-
-            return b.SingleOrDefault();
         }
 
         [System.Serializable]
