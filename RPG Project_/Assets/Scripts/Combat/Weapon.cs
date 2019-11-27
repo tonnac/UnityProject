d65defe46543e2880bdd4c994a5671ca4a1ad937
tonnac <tonnac@ilati.com>
2019-11-27 15:22:34 +0900

    175. Boom Microphont Audio Listener

--- RPG Project_/Assets/Scripts/Combat/Weapon.cs
+++ RPG Project_/Assets/Scripts/Combat/Weapon.cs
@@ -1,12 +1,15 @@
 namespace RPG.Combat
 {
     using UnityEngine;
-    
+    using UnityEngine.Events;
+
     public class Weapon : MonoBehaviour 
     {
+        [SerializeField] UnityEvent onHit;
         public void OnHit()
         {
             print($"Weapon Hit {gameObject.name}");
+            onHit.Invoke();
         }
     }
 }
